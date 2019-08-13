f205ebe24d049af143c6a78c0f972dae175e8699
jianglin521 <17610835880@163.com>
2019-08-13 11:10:53 +0800

    添加mongodb文件

---
+++ day05/00.js
@@ -0,0 +1,16 @@
+/* Created by guan on 2019/8/9 */
+var express = require('express')
+var app = express()
+
+var myLogger = function (req, res, next) {
+  console.log('LOGGED')
+  next()
+}
+
+app.use(myLogger)
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000)
